f32c205bed64270222f42dc5084591ff12e9b3f2
andrjre <andrjre@gmail.com>
2025-03-30 21:24:06 +0100

    Style changes and buttons added.

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 
 
 let box = document.getElementById("box")
-
-let row;
-let col;
+let red = document.getElementById("red")
 
 
+
+document.getElementById("red").onClick = changeRed;
+
+    function changeRed (){
+        console.log("heelolo")
+        box.style.color = "red"
+    }
+
+    // find out how to change colors //
